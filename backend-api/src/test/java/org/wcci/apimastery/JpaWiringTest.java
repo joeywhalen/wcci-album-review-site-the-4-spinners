da959685b31f78b53fa99b31c0178d462bfb3b0e
Joey Whalen <jtktwhalen@gmail.com>
2021-07-25 14:00:20 -0400

    Building JpaWiringTest.  Added @Autowired.

--- backend-api/src/test/java/org/wcci/apimastery/JpaWiringTest.java
+++ backend-api/src/test/java/org/wcci/apimastery/JpaWiringTest.java
@@ -1,5 +1,23 @@
 package org.wcci.apimastery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.wcci.apimastery.storage.AlbumRepository;
+import org.wcci.apimastery.storage.ListRepository;
+import org.wcci.apimastery.storage.SongRepository;
 
+@DataJpaTest
 public class JpaWiringTest {
+
+    @Autowired
+    private ListRepository listRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private AlbumRepository albumRepository;
+    @Autowired
+    private SongRepository songRepository;
+
+    
 }
