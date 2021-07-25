df16e293f1f8c358535aaf1c7d60a3c111ab43f3
Joey Whalen <jtktwhalen@gmail.com>
2021-07-25 14:46:03 -0400

    Building JpaWiringTest.  Added Save And Retrieve Song By ID which passed.

--- backend-api/src/test/java/org/wcci/apimastery/JpaWiringTest.java
+++ backend-api/src/test/java/org/wcci/apimastery/JpaWiringTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.wcci.apimastery.resources.Album;
 import org.wcci.apimastery.resources.List;
+import org.wcci.apimastery.resources.Song;
 import org.wcci.apimastery.storage.AlbumRepository;
 import org.wcci.apimastery.storage.ListRepository;
 import org.wcci.apimastery.storage.SongRepository;
@@ -24,16 +25,10 @@ public class JpaWiringTest {
     @Autowired
     private SongRepository songRepository;
 
-    private void flushAndClear() {
-        entityManager.flush();
-        entityManager.clear();
-    }
-
     @Test
     public void listRepositoryShouldSaveAndRetrieveListObjects() {
         List testList = new List("listName");
         listRepository.save(testList);
-//        flushAndClear();
         List retrievedList = listRepository.findById(testList.getId()).get();
         assertThat(retrievedList).isEqualTo(testList);
     }
@@ -44,8 +39,18 @@ public class JpaWiringTest {
         Album testAlbum = new Album(testList, "Album title","Artist", "WWW.com",
                 "Record label", "90", 5,"video.com");
         albumRepository.save(testAlbum);
-//        flushAndClear();
         Album retrievedAlbum = albumRepository.findById(testAlbum.getId()).get();
         assertThat(retrievedAlbum).isEqualTo(testAlbum);
+    }
+
+    @Test
+    public void songRepoShouldSaveAndRetrieveSongObjects() {
+        List testList = new List("listName");
+        Album testAlbum = new Album(testList, "Album title","Artist", "WWW.com",
+                "Record label", "90", 5,"video.com");
+        Song testSong = new Song(testAlbum, "sing song", "90", 5);
+        songRepository.save(testSong);
+        Song retrievedSong = songRepository.findById(testSong.getId()).get();
+        assertThat(retrievedSong).isEqualTo(testSong);
     }
 }
