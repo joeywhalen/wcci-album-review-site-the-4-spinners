d87f364a47979af10ab29349deb5541d0d221f83
Joey Whalen <jtktwhalen@gmail.com>
2021-07-25 16:14:51 -0400

    Created lists-requests.http file.

--- backend-api/src/main/java/org/wcci/apimastery/controllers/ListController.java
+++ backend-api/src/main/java/org/wcci/apimastery/controllers/ListController.java
@@ -1,4 +1,22 @@
 package org.wcci.apimastery.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.wcci.apimastery.storage.AlbumRepository;
+import org.wcci.apimastery.storage.ListStorage;
+import org.wcci.apimastery.storage.SongRepository;
+
+@RestController
 public class ListController {
+
+    private ListStorage listStorage;
+    private AlbumRepository albumRepository;
+    private SongRepository songongRepository;
+
+    public ListController(ListStorage listStorage, AlbumRepository albumRepository, SongRepository songongRepository) {
+        this.listStorage = listStorage;
+        this.albumRepository = albumRepository;
+        this.songongRepository = songongRepository;
+    }
+
+
 }
