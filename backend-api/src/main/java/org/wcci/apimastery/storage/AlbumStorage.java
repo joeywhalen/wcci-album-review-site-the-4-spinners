83360547d4e7b114df7420796af0f89aad71c10e
George Fought <84338581+GeorgeFought@users.noreply.github.com>
2021-07-27 10:25:45 -0400

    Refactored references of album and song storage in populator.

--- backend-api/src/main/java/org/wcci/apimastery/storage/AlbumStorage.java
+++ backend-api/src/main/java/org/wcci/apimastery/storage/AlbumStorage.java
@@ -1,29 +1,29 @@
-package org.wcci.apimastery.storage;
-
-import org.wcci.apimastery.resources.Album;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AlbumStorage {
-    private AlbumRepository albumRepository;
-
-    public AlbumStorage(AlbumRepository albumRepository){
-        this.albumRepository = albumRepository;
-    }
-
-    public Album retrieveAlbumById(Long id){
-        return albumRepository.findById(id).get();
-    }
-
-    public Iterable<Album> retrieveAllAlbums(){
-        return albumRepository.findAll();
-    }
-
-    public void saveAlbum(Album albumToSave){
-        albumRepository.save(albumToSave);
-    }
-
-    public void deleteAlbumById(Long id){
-        albumRepository.deleteById(id);
-    }
-}
+//package org.wcci.apimastery.storage;
+//
+//import org.wcci.apimastery.resources.Album;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AlbumStorage {
+//    private AlbumRepository albumRepository;
+//
+//    public AlbumStorage(AlbumRepository albumRepository){
+//        this.albumRepository = albumRepository;
+//    }
+//
+//    public Album retrieveAlbumById(Long id){
+//        return albumRepository.findById(id).get();
+//    }
+//
+//    public Iterable<Album> retrieveAllAlbums(){
+//        return albumRepository.findAll();
+//    }
+//
+//    public void saveAlbum(Album albumToSave){
+//        albumRepository.save(albumToSave);
+//    }
+//
+//    public void deleteAlbumById(Long id){
+//        albumRepository.deleteById(id);
+//    }
+//}
