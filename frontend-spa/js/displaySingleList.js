ed67f91fa1b64837e6d538293915dc609e66596c
nyirenkyi17 <nyirenkyi17@gmail.com>
2021-07-28 16:30:28 -0400

    added display single list

---
+++ frontend-spa/js/displaySingleList.js
@@ -0,0 +1,20 @@
+import { clearChildren } from "./displayHome";
+import { displayHome } from "./displayHome";
+
+
+const displaySingleList = function (mainElement){
+    clearChildren (mainElement);
+    list.albums.forEach((album) =>{
+      let albumElement = document.createdElement("section");
+      albumElement.classList.add("btn-section");
+      let albumNameElement = doucment.createdElement("h3");
+      albumNameElement.innerText = album.title;
+      let albumImage = document.createElement("div");
+      albumImage.innerHTML = '<a href="' + album.imageURL + '"  alt="Album Art"></a>'
+      albumElement.appendChild(albumNameElement);
+      albumElement.appendChild(albumImage);
+      mainElement.appendChild(albumElement);
+    });
+    return mainElement;
+}
+export {displaySingleList}
