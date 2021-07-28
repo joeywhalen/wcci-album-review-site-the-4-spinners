2df16fc517a629879c8cd5b58a6ae2bad9290001
George Fought <84338581+GeorgeFought@users.noreply.github.com>
2021-07-28 11:26:02 -0400

    Commented out header, footer works.

---
+++ frontend-spa/js/app.js
@@ -0,0 +1,7 @@
+// import { createHeader } from "./header.js";
+import { createFooter } from "./footer.js";
+
+const container = document.querySelector(".container");
+// container.append(createHeader());
+container.append(createFooter());
+console.log("test");
