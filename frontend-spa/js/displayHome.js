14cae753f2a5b0dccd35e90baafad10e1823ffbb
HEWiggins <HE.Wiggins1990@gmail.com>
2021-07-28 15:03:46 -0400

    created displayHome.js & clearChildren. Imported displayHome to apps.js

--- frontend-spa/js/displayHome.js
+++ frontend-spa/js/displayHome.js
@@ -1,2 +1,16 @@
 
 
+const displayHome = function(mainElement) {
+    clearChildren(mainElement);
+    mainElement.innerHTML = `<img src="/frontend-spa/images/Logo.png" alt="Desert Island Logo">`
+    // mainElement.appendChild();
+    return mainElement;
+   
+   
+}
+const clearChildren = function(element){
+    while(element.firstChild){
+        element.removeChild(element.lastChild);
+    }
+};
+export {displayHome, clearChildren};
