79294d53f5ad53a05aa619a812bdf79ae3e9a997
George Fought <84338581+GeorgeFought@users.noreply.github.com>
2021-07-28 12:19:17 -0400

    started listBar.js and edditing the fetch in app.js

--- frontend-spa/js/app.js
+++ frontend-spa/js/app.js
@@ -1,7 +1,16 @@
 import { createHeader } from "./header.js";
 import { createFooter } from "./footer.js";
+import { createListBar } from "./listBar.js";
 
 const container = document.querySelector(".container");
 container.append(createHeader());
+container.append(createListBar(lists));
+
+fetch("http://localhost:8080/api/lists")
+    .then(response => response.json())
+    .then(lists => createListBar(lists))
+    .then(listElement => listBar.appendChild(listElement))
+    .catch(error => console.log(error));
+    
 container.append(createFooter());
 console.log("test");
