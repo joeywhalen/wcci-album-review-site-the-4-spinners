374ee387f26e15551e69bd7723bf64f3b38622d4
Joey Whalen <jtktwhalen@gmail.com>
2021-07-29 14:07:37 -0400

    Adding new album form to single list view.

--- frontend-spa/js/displaySingleList.js
+++ frontend-spa/js/displaySingleList.js
@@ -20,6 +20,35 @@ const displaySingleList = function (list){
       albumElement.appendChild(albumLink);
       mainElement.appendChild(albumElement);
     });
+  
+  const form = document.createElement("form");
+  form.classList.add("new-album-form");
+  const listInput = document.createElement("input");
+  listInput.classList.add("attach-list-name");
+  listInput.setAttribute("type", "text");
+  listInput.setAttribute("placeholder", "Which list?...");
+  const titleInput = document.createElement("input");
+  titleInput.classList.add("new-album-title");
+  titleInput.setAttribute("type", "text");
+  titleInput.setAttribute("placeholder", "Title of Album...");
+  const artistInput = document.createElement("input");
+  artistInput.classList.add("new-album-artist");
+  artistInput.setAttribute("type", "text");
+  artistInput.setAttribute("placeholder", "Album artist...");
+  const imageURLInput = document.createElement("input");
+  imageURLInput.classList.add("new-album-cover-image");
+  imageURLInput.setAttribute("type", "text");
+  imageURLInput.setAttribute("placeholder", "Album cover image URL...");
+  const recordLabelInput = document.createElement("input");
+  recordLabelInput.classList.add("new-album-record-label");
+  recordLabelInput.setAttribute("type", "text");
+  recordLabelInput.setAttribute("placeholder", "Album record label...");
+  const durationInput = document.createElement("input");
+  durationInput.classList.add("new-album-duration");
+  durationInput.setAttribute("type", "text");
+  durationInput.setAttribute("placeholder", "Album duration...");
+  const starRatingInput = document.createElement("input");
+  
     return mainElement;
 }
 export {displaySingleList}
