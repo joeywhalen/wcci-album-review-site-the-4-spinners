40cf524dbc33731365ceb5714258708ae377f333
George Fought <84338581+GeorgeFought@users.noreply.github.com>
2021-07-29 12:13:53 -0400

    Removed header.js and refrences to it.

--- frontend-spa/js/app.js
+++ frontend-spa/js/app.js
@@ -1,14 +1,9 @@
-import { createHeader } from "./header.js";
 import { createFooter } from "./footer.js";
-//import { allListJson } from "./sampleListJson.js";
 import { createListBar } from "./listBar.js";
 import { displayHome } from "./displayHome.js";
 
 
 const container = document.querySelector(".container");
-// container.append(createHeader());
-
-// container.append(createListBar(allListJson));
 
 await fetch("http://localhost:8080/api/lists")
     .then(response => response.json())
