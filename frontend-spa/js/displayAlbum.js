afed41e9e015516be52109251ff1d598a985ce44
Joey Whalen <jtktwhalen@gmail.com>
2021-08-01 14:23:50 -0400

    Added delete button and fetch.

--- frontend-spa/js/displayAlbum.js
+++ frontend-spa/js/displayAlbum.js
@@ -57,8 +57,24 @@ const displayAlbum = function (album) {
     albumRetitleForm.appendChild(albumRetitleInput);
     albumRetitleForm.appendChild(submitRetitleAlbumButton);
 
+    const deleteAlbumButton = document.createElement("button");
+    deleteAlbumButton.classList.add("delete-album");
+    deleteAlbumButton.innerText = "Delete this Album";
+
+    deleteAlbumButton.addEventListener("click", (clickEvent) => {
+        clickEvent.preventDefault();
+        fetch("http://localhost:8080/api/lists/" + album.listId + "/albums/" + album.id, {
+            method: "DELETE",
+        })
+            .then(response => response.json())
+            .then(listResponse => displaySingleList(listResponse))
+            .catch(error => console.log(error));
+    })
+
+
     albumElement.appendChild(albumTitleElement);
     albumElement.appendChild(albumRetitleForm);
+    albumElement.appendChild(deleteAlbumButton);
     albumElement.appendChild(albumImageElement);
     albumElement.appendChild(albumArtistElement);
     albumElement.appendChild(recordLabelElement);
